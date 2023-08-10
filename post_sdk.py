f90c6aa023f9ee8367a4237e950907ffc4f1964c
MoeX <54111871+MengXin001@users.noreply.github.com>
2023-08-10 20:35:57 +0800

    feat: debug mode

--- post_sdk.py
+++ post_sdk.py
@@ -7,21 +7,20 @@ from read_gps import *
 from bme import *
 from libs import *
 
-a = 1
-while a > 0:
-  try:
+while True:
     now = datetime.datetime.now()
     timestamp = now.timestamp()
-    lon, lat, gps_h = read_geo()
-    gps_speed = read_spd()
-
+    #lon, lat, gps_h = read_geo()
+    lon, lat, gps_h = 113.9570, 22.5390, 25
+    #gps_speed = read_spd()
+    gps_speed = 0
     temperature,pressure,humidity = readBME280All()
     temperature = rounddata(temperature,1)
     pressure = rounddata(pressure,1)
     humidity = rounddata(humidity,1)
     dt = rounddata(dewPointFast(temperature,humidity),1)
     tmslp = rounddata(mslp(pressure,temperature,gps_h),1)
-    
+
     std_json = {
     "timestamp": timestamp,
     "instrument": "tqm Station",
@@ -38,12 +37,11 @@ while a > 0:
             "lat": lat, "long": 113.939, "alt": gps_h, "dir": 0, "speed": gps_speed
           },
             }
-
-    resp = requests.post(
+    s = requests.session()
+    s.keep_alive = False
+    resp = s.post(
                     url='http://track.storm-chasers.cn/api/receive_meteo_data_json',
                     data=json.dumps(std_json)
                     )
     print(resp.json)
-  except:
-     print("error")
-  time.sleep(20)
+    time.sleep(10)
